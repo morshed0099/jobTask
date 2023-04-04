64e85ffeff32b6d50f596f4959331b0544c883e9
morshed0099 <mdgolammorshed0099@gmail.com>
2023-04-04 10:26:01 +0600

    modify counter page set end time

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -15,9 +15,9 @@ export default function Counter() {
       day = hour * 24;
 
     let clockInterval = setInterval(() => {
-      const end = new Date("Jul 05, 2023 12:00:00").getTime();
+      const end = new Date("jul 04, 2023 10:10:00").getTime();
       const current = new Date().getTime();
-      const remaining = end-current;
+      const remaining = end - current;
 
       let dd = Math.floor(remaining / day);
       let hh = Math.floor((remaining % day) / hour);
@@ -32,29 +32,33 @@ export default function Counter() {
     return () => {
       clearInterval(clockInterval);
     }
-  }, []);
+  }, []); 
+      
+       if(clockData.dd + clockData.hh + clockData.mm + clockData.ss <= 0 )
+       { return <h1>Date expried</h1>}else       
+      
+      {return <div className="flex-w flex-c-m cd100 p-b-33">
+        <div className="flex-col-c-m size2 bor1 m-l-15 m-r-15 m-b-20">
+          <span className="l2-txt1 p-b-9 days">{clockData.dd}</span>
+          <span className="s2-txt1">Days</span>
+        </div>
 
-  return (
-    <div className="flex-w flex-c-m cd100 p-b-33">
-      <div className="flex-col-c-m size2 bor1 m-l-15 m-r-15 m-b-20">
-        <span className="l2-txt1 p-b-9 days">{clockData.dd}</span>
-        <span className="s2-txt1">Days</span>
-      </div>
+        <div className="flex-col-c-m size2 bor1 m-l-15 m-r-15 m-b-20">
+          <span className="l2-txt1 p-b-9 hours">{clockData.hh}</span>
+          <span className="s2-txt1">Hours</span>
+        </div>
 
-      <div className="flex-col-c-m size2 bor1 m-l-15 m-r-15 m-b-20">
-        <span className="l2-txt1 p-b-9 hours">{clockData.hh}</span>
-        <span className="s2-txt1">Hours</span>
-      </div>
+        <div className="flex-col-c-m size2 bor1 m-l-15 m-r-15 m-b-20">
+          <span className="l2-txt1 p-b-9 minutes">{clockData.mm}</span>
+          <span className="s2-txt1">Minutes</span>
+        </div>
 
-      <div className="flex-col-c-m size2 bor1 m-l-15 m-r-15 m-b-20">
-        <span className="l2-txt1 p-b-9 minutes">{clockData.mm}</span>
-        <span className="s2-txt1">Minutes</span>
-      </div>
+        <div className="flex-col-c-m size2 bor1 m-l-15 m-r-15 m-b-20">
+          <span className="l2-txt1 p-b-9 seconds">{clockData.ss}</span>
+          <span className="s2-txt1">Seconds</span>
+        </div>
+      </div>}
+    
+  
 
-      <div className="flex-col-c-m size2 bor1 m-l-15 m-r-15 m-b-20">
-        <span className="l2-txt1 p-b-9 seconds">{clockData.ss}</span>
-        <span className="s2-txt1">Seconds</span>
-      </div>
-    </div>
-  );
 }
